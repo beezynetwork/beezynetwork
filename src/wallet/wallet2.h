7bbdc38b4db67171b80737ce079b06635556dbb9
beezynetwork <netzwerk@beezyinmotion.de>
2023-12-23 21:34:40 +0100

    Update wallet2.h
    changes 1,74

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -6,6 +6,11 @@
 
 #pragma once
 #include <memory>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/version.hpp>
+#if BOOST_VERSION / 100000 == 1 && BOOST_VERSION / 100 % 1000 == 74
+#include <boost/serialization/library_version_type.hpp>
+#endif
 #include <boost/serialization/list.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/deque.hpp>
