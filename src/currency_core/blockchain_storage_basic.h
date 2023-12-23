2ab5f50ef5b385f5bd6a8cc4249a155972bf662f
beezynetwork <netzwerk@beezyinmotion.de>
2023-12-23 21:00:21 +0100

    Update blockchain_storage_basic.h
    fix for boost 1,74 added

--- src/currency_core/blockchain_storage_basic.h
+++ src/currency_core/blockchain_storage_basic.h
@@ -14,6 +14,9 @@
 
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/version.hpp>
+#if BOOST_VERSION / 100000 == 1 && BOOST_VERSION / 100 % 1000 == 74
+#include <boost/serialization/library_version_type.hpp>
+#endif
 #include <boost/serialization/list.hpp>
 
 #include "currency_basic.h"
